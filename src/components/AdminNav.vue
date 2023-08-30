b0136c049377150ae492264d0b57ad74854d5793
mvazquezma <mvm51090@gmail.com>
2023-08-30 12:32:10 +0200

    Create admin nav and admin layout

--- src/components/AdminNav.vue
+++ src/components/AdminNav.vue
@@ -6,14 +6,14 @@
 <template>
     <header class="px-10 py-5 bg-gray-700 flex justify-between absolute top-0 w-full z-10">
         <div>
-           <Logo />
+            <Logo />
         </div>
 
         <nav>
             <Link
-                to="admin"    
+                to="shop"    
             >
-                Administrar
+                Ir a tienda
             </Link>
         </nav>
     </header>
