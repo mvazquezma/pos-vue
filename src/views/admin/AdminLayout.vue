c60d359d9502de6f708a4ceb26d4255460bfdebb
mvazquezma <mvm51090@gmail.com>
2023-08-31 12:30:33 +0200

    Instal formkit and add fields to new product page

--- src/views/admin/AdminLayout.vue
+++ src/views/admin/AdminLayout.vue
@@ -5,7 +5,7 @@
 <template>
     <AdminNav />
 
-    <div class="pt-32 container mx-auto lg:min-h-screen">
+    <div class="pt-32 container mx-auto lg:min-h-screen ">
         <RouterView />
     </div>
 </template>
