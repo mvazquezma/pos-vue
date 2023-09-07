08a234485bebf929ec6fb71e478577e753f8dfa7
mvazquezma <mvm51090@gmail.com>
2023-09-07 11:52:45 +0200

    Calculate subtotal

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -27,7 +27,7 @@ import { formatCurrency } from '../helpers';
         <dl class="space-y-6 border-t border-gray-200 pt-6 text-sm font-medium text-gray-500">
             <Amount>
                 <template #label>Subtotal:</template>
-                {{ formatCurrency(300) }}
+                {{ formatCurrency(cart.subtotal) }}
             </Amount>
 
             <Amount>
--- src/stores/cart.js
+++ src/stores/cart.js
@@ -1,11 +1,19 @@
+import { ref, computed, watch } from "vue";
 import { defineStore } from "pinia";
-import { ref, computed } from "vue";
+
 
 const items = ref([])
+const subtotal = ref(0)
 
 const MAX_PRODUCTS = 5
 
 export const useCartStore = defineStore('cart', () => {
+
+    watch(items, () => {
+        subtotal.value = items.value.reduce((total, item) => total + (item.quantity * item.price), 0)
+    },{
+        deep: true
+    })
 
     function addItem(item) {
         items.value.push({...item, quantity: 1, id: item.id})
@@ -23,6 +31,7 @@ export const useCartStore = defineStore('cart', () => {
 
     return {
         items,
+        subtotal,
         addItem,
         updateQuantity,
         isCartEmpty,
