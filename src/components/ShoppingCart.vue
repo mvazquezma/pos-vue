05fc3052bff4dde8b35330917b56c8b99ffc107b
mvazquezma <mvm51090@gmail.com>
2023-09-07 11:42:01 +0200

    Create amount component to show different quantities

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,6 +1,8 @@
 <script setup>
     import ShoppingCartItem from './ShoppingCartItem.vue'
     import { useCartStore } from '../stores/cart';
+    import Amount from './Amount.vue';
+import { formatCurrency } from '../helpers';
 
     const cart = useCartStore()
 </script>
@@ -21,5 +23,22 @@
                 :item="item"
             />
         </ul>
+
+        <dl class="space-y-6 border-t border-gray-200 pt-6 text-sm font-medium text-gray-500">
+            <Amount>
+                <template #label>Subtotal:</template>
+                {{ formatCurrency(300) }}
+            </Amount>
+
+            <Amount>
+                <template #label>Impuestos:</template>
+                {{ formatCurrency(0) }}
+            </Amount>
+
+            <Amount>
+                <template #label>Total a Pagar:</template>
+                {{ formatCurrency(0) }}
+            </Amount>
+        </dl>
     </div>
 </template>
